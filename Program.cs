aab84e03f1857061b650ae173bb29ed7b81541cf
jamesdlc <jamesdelacruz@gmail.com>
2016-12-29 14:28:02 -0800

    re initialized repo

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type conversion practice
+            byte b = 1;
+            int i = b;
+            Console.WriteLine(i);
+        }
+    }
+}
